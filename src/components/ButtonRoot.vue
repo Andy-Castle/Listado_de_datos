6dc51f1e5f9ae9ec7210e8afc1e7cf771f22e37b
Andy-Castle <masterchief_and@live.com>
2024-02-10 18:26:10 -0500

    .

---
+++ src/components/ButtonRoot.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="GoToRoot">Ir a la raíz</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const GoToRoot = () => {
+  router.push('/')
+}
+</script>
