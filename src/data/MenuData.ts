7f665c4f74021e251d8e45027a5afce1eef489e8
Andy-Castle <masterchief_and@live.com>
2024-02-10 20:37:21 -0500

    .

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -6,6 +6,36 @@ const MenuData: IMenu[] = [
     title: 'FirstView',
     src: '/src/views/FirstView.vue',
     path: '/First'
+  },
+  {
+    id: 2,
+    title: 'SecondView',
+    src: '/src/views/SecondView.vue',
+    path: '/Second'
+  },
+  {
+    id: 3,
+    title: 'ThirdView',
+    src: '/src/views/ThirdView.vue',
+    path: '/Third'
+  },
+  {
+    id: 4,
+    title: 'FourthView',
+    src: '/src/views/FourthView.vue',
+    path: '/Fourth'
+  },
+  {
+    id: 5,
+    title: 'FifthView',
+    src: '/src/views/FifthView.vue',
+    path: '/Fifth'
+  },
+  {
+    id: 6,
+    title: 'SixthView',
+    src: '/src/views/SixthView.vue',
+    path: '/Sixth'
   }
 ]
 
