953c02eff5134ea4a5a444877ff7e28fc9d9f3cd
Andy-Castle <masterchief_and@live.com>
2024-02-10 19:32:55 -0500

    .

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button @click="toggleImage">{{ mostrarImagen ? 'Ocultar Imagen' : 'Mostrar Imagen' }}</button>
+    <button class="imageButton" @click="toggleImage">
+      {{ mostrarImagen ? 'Ocultar Imagen' : 'Mostrar Imagen' }}
+    </button>
     <img v-if="mostrarImagen" src="/src/assets/images/Evan_You.jpg" alt="Evan You" />
   </div>
 </template>
@@ -18,5 +20,22 @@ const toggleImage = () => {
 <style scoped>
 img {
   width: 400px;
+}
+
+.imageButton {
+  padding: 10px 15px;
+  background-color: #280274;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.imageButton:hover {
+  background-color: #3652ad;
+}
+
+.imageButton:active {
+  background-color: #280274;
 }
 </style>
--- src/views/SixthView.vue
+++ src/views/SixthView.vue
@@ -4,9 +4,31 @@ import ImageComponent from '@/components/ImageComponent.vue'
 </script>
 
 <template>
-  <ButtonRoot />
-  <h1>SixthView</h1>
-  <ImageComponent />
+  <div class="container">
+    <h1>SixthView</h1>
+    <div class="buttons">
+      <ButtonRoot />
+      <ImageComponent />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 30px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  text-align: center;
+}
+
+.buttons {
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+</style>
